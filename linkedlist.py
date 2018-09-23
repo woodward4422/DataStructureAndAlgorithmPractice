d20aa29fdf9776b49a91ef83e4904b34a58015cd
noah <noahwoodward2244@gmail.com>
2018-09-23 15:30:59 -0700

    added implementation of queues

--- linkedlist.py
+++ linkedlist.py
@@ -1,64 +1,63 @@
 class Element(object):
-    def __init__(self,value):
-        self.value = value 
-        self.next = None 
-
+        def __init__(self,value):
+            self.value = value
+            self.next = None
+    
 class LinkedList(object):
-    def __init__(self, head=None):
-        self.head = head
-
-    def append(self, new_element):
-        current = self.head
-        counter = 0 
-        if self.head:
-            while current.next:
-                current = current.next
-            current.next = new_element
-        else:
-            self.head = new_element
+        def __init__(self,head = None):
+            self.head = head
     
-    def get_position(self,position):
-        current = self.head
-        counter = 1
-        if position < 1: 
-            return None
-        while current and counter <= position:
-            if counter == position:
-                return current 
-            current = current.next
-            counter += 1
-        return None 
+        def append(self,new_element):
+            current = self.head
+            if self.head:
+                while current.next:
+                    current = current.next
+                current.next = new_element
+            else:
+                self.head = current
     
-    def insert(self,new_element,position):
-        counter = 1
-        current = self.head
-        if position > 1:
-            while current and counter < position:
-                if counter == position - 1:
-                    new_element.next = current.next
-                    current.next = new_element
+        def get_position(self, position):
+            counter = 1
+            current = self.head
+            if position < 1:
+                return None
+            while current and counter <= position:
+                if counter == position:
+                    return current
                 current = current.next
                 counter += 1
-        elif position == 1:
-            new_element.next = self.head
-            self.head = new_element
-  
-  
-    
-    def delete(self,value):
-        current = self.head 
-        previous = None 
-        
-        while current.value != value:
-            previous = current
-            current = current.next 
+            return None
 
+        def insert(self, new_element, position):
+            counter = 1
+            current = self.head
+            if position > 1:
+                while current and counter < position:
+                    if counter == position - 1:
+                        new_element.next = current.next
+                        current.next = new_element
+                    current = current.next
+                    counter += 1
+            elif position == 1:
+                new_element.next = self.head
+                self.head = new_element
+    
+        def delete(self,value):
+            current = self.head 
+            previous = None 
+            while current.value != value:
+                previous = current
+                current = current.next
+             
             if current.value == value:
-                if previous: 
+                if previous:
                     previous.next = current.next 
                 else:
                     self.head = current.next
-    
+            
+        
+
+
     
     
 
